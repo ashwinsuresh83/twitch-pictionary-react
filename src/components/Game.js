941527a7c672a2a0aba642f8aeb3cd2628d5beb5
Ayush Sharma <ayushlivelife@gmail.com>
2021-04-22 23:32:00 +0530

    update

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import '../css/game.css'
+import '../css/home.css'
 const tmi = require('tmi.js');
 export default function Game() {
     const message=""
@@ -13,19 +14,22 @@ export default function Game() {
     client.connect();
     
     client.on('message', (channel, tags, message, self) => {
+
         // "Alca: Hello, World!"
         message=`${tags['display-name']}: ${message}`
+       
         document.querySelector('.message').innerText=message
-        
+
     });
     
     return (
         
-        <div>
+        <div class='home__page'>
             
             <h1>{twitch__name}</h1>
-
+            <div>
             <h3 className="message"> empty</h3>
+            </div>
         </div>
     )
 }
